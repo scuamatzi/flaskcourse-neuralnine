ad21cb0804e3e93a871386296d720332250cb630
salvador <escuamatzi@gmail.com>
2025-08-13 18:02:58 -0600

    Worked with forms, files and post method, very basic but useful. neuralnine video on youtube worked with excel files using pandas... I will try it later

--- app.py
+++ app.py
@@ -1,43 +1,31 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, request
 
 app = Flask(__name__, template_folder="templates")
 
 
-@app.template_filter("reverse_string")
-def reverse_string(data):
-    return data[::-1]
-
-
-@app.template_filter("repeat")
-def repeat(data, times=2):
-    return data * times
-
-
-@app.template_filter("alternate_case")
-def alternate_case(data):
-    return "".join([c.upper() if i % 2 == 0 else c.lower() for i, c in enumerate(data)])
-
-
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def index():
-    title = "Flask course"
-    description = "NeuralNine course from Novice to Advanced"
-    ages_list = [25, 8, 35, 9, 11, 27, 42, 6, 59, 24]
-    return render_template(
-        "index.html", title=title, description=description, ages_list=ages_list
-    )
+    if request.method == "GET":
+        return render_template("index.html")
+    elif request.method == "POST":
+        username = request.form["username"]
+        password = request.form.get("password")
+        if username == "yo" and password == "tu":
+            return "Success"
+        else:
+            return "Failure"
 
 
-@app.route("/filters")
-def filters():
-    data = "Lorem ipsum"
-    return render_template("filters.html", data=data)
+@app.route("/file_upload", methods=["POST"])
+def file_upload():
+    file = request.files["file"]
 
-
-@app.route("/redirect_endpoint")
-def redirect_endpoint():
-    return redirect(url_for("filters"))
+    if file.content_type == "text/plain":
+        return file.read().decode()
+    else:
+        return "Not text plain"
 
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5555, debug=True)
+
