3c7e077c780aed264b09d6eabd34d0f1504237ec
salvador <escuamatzi@gmail.com>
2025-08-14 17:46:02 -0600

    Worked with static files for images, css files and js files

--- app.py
+++ app.py
@@ -1,31 +1,14 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template
 
-app = Flask(__name__, template_folder="templates")
+app = Flask(
+    __name__, template_folder="templates", static_folder="static", static_url_path="/"
+)
 
 
-@app.route("/", methods=["GET", "POST"])
+@app.route("/")
 def index():
-    if request.method == "GET":
-        return render_template("index.html")
-    elif request.method == "POST":
-        username = request.form["username"]
-        password = request.form.get("password")
-        if username == "yo" and password == "tu":
-            return "Success"
-        else:
-            return "Failure"
-
-
-@app.route("/file_upload", methods=["POST"])
-def file_upload():
-    file = request.files["file"]
-
-    if file.content_type == "text/plain":
-        return file.read().decode()
-    else:
-        return "Not text plain"
+    return render_template("index.html")
 
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5555, debug=True)
-
