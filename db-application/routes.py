21ad2a3737b5efa2bd14a3c3a115fe0162f463c4
salvador <escuamatzi@gmail.com>
2025-09-08 13:29:25 -0600

    Second part of database. Now it has delete option and details option for the users. Finished with a basic html format

--- db-application/routes.py
+++ db-application/routes.py
@@ -21,3 +21,16 @@ def register_routes(app, db):
 
             people = Person.query.all()
             return render_template("index.html", people=people)
+
+    @app.route("/delete/<pid>", methods=["DELETE"])
+    def delete(pid):
+        Person.query.filter(Person.pid == pid).delete()
+        db.session.commit()
+
+        people = Person.query.all()
+        return render_template("index.html", people=people)
+
+    @app.route("/details/<pid>")
+    def details(pid):
+        person = Person.query.filter(Person.pid == pid).first()
+        return render_template("details.html", person=person)
